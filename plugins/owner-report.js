81aa4ed245d0746e87382ad7b582ad93f82d8e41
hamidanoo <133199761+hamidanoo@users.noreply.github.com>
2023-11-09 04:36:24 -0800

    Update owner-report.js

--- plugins/owner-report.js
+++ plugins/owner-report.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*Enter A Message*\n\n*📌Example:*\n*${usedPrefix + command} The Command ${usedPrefix}Play Not Working*`;
   if (text.length < 10) throw `*⚠️Must Need 10 Characters To Send Report!*`;
-  if (text.length > 1000) throw `*❌Too Long Only 1000 Characters Allowed!*`;
+  if (text.length > 500) throw `*❌Too Long Only 1000 Characters Allowed!*`;
   const teks = `*╭───❮ REPORT ❯*\n*│*\n*│ Number:* wa.me/${m.sender.split`@`[0]}\n*│*\n*│*\n*│ Message:* ${text}\n*╰────────────⦁*`;
-  conn.reply('919074692450@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('919074692450@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  m.reply(`*✅Report Sent To The Developer Of This Bot. If This Is A Joke Or False It Will Be Ignored*`);
+  conn.reply('923039573226@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('923039573226@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  m.reply(`*✅Report sended to Hmaid-Hussain if this is joke it will be ignored*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
 handler.tags = ['info'];
