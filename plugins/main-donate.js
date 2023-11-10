6052f7596536ec71e90f21a3e99249a9ee32d5a5
hamidanoo <133199761+hamidanoo@users.noreply.github.com>
2023-11-10 02:39:32 -0800

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -2,8 +2,9 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
-≡ *SUPPOERT*
-Share BLACK Bot Link And Support. Git Link https://github.com/hamidanoo/BLACK-56`
+≡ *SUPPORT*
+_Join Black Support Group_ 
+Link https://chat.whatsapp.com/CAB2fLbnv1wJFFY9lqFH81`
 let img = 'https://replicate.delivery/pbxt/CJyMWxJtxTL2O5nFrc3askbFihaLccVBBahyVdFpEV4RElcE/out..jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
